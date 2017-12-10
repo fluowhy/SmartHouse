086552e4a7441dfa1f6dfd429a1266f1e4344643
mauricio romero <mau.altgr@hotmail.com>
2017-12-10 15:16:36 -0300

    Add files via upload

--- arduino.py
+++ arduino.py
@@ -2,9 +2,9 @@
 
 from __future__ import division
 import numpy as np
-import matplotlib.pyplot as plt
 import time
 import pyfirmata
+import datetime
 
 # Este programa lee los pines analogico 0 y digital 2 de un arduino uno a traves
 # del puerto USB y guarda un archivo de texto con la informacion del estado de 
@@ -26,32 +26,34 @@ it = pyfirmata.util.Iterator(board) # para que el buffer no haga overflow
 it.start()
 
 A0 = board.get_pin('a:0:i') # define pin analogico A0 como input
-D2 = board.get_pin('d:2:i') # define pin digital D2 como input (ampolleta 1)
-D3 = board.get_pin('d:3:i') # ampolleta 2
-dt = 10 # tiempo de actualizacion en segundos
+D2 = board.get_pin('d:2:o') # define pin digital D2 como input (ampolleta 1)
+dt1 = 1 # tiempo de actualizacion en segundos
 t1 = time.time() # toma tiempo inicial
-while True:	
+state = 0
+while True:
 	t2 = time.time() # toma tiempo final
 	dt = t2 - t1 # calcula dt
-	if dt >= 10: # si dt > 10 segundos calcula estado de las luces
+	if dt >= dt1: # si dt > 10 segundos calcula estado de las luces
 		k = 0 # muestras reales (hay problema con muestras None)
-		a = 0 # guarda estado pin analogico (prueba)
-		b = 0 # guarda estado pin digital
-		c = 0
+		lec = 0 # guarda estado pin analogico (prueba)
 		for i in range(10): # el estado de las luces se calcula con un promedio de 10 muestras
-			b1 = D2.read() # lee pin digital 2
-			a1 = A0.read() # lee pin analogico 0 
-			c1 = D3.read() # lee pin digital 3
-			if a1!=None and b1!=None: # condicion para que no hayan NoneType
+			a = A0.read() # lee pin analogico 0 
+			if a!=None: # condicion para que no hayan NoneType
 				k += 1
-				a += a1
-				b += b1
-				c += c1
-		if k!=0:
-			print b/k # print valor digital
-			print c/k
-			print (5 - a/k*5)*2000/(a/k*5), 'Ohm' # print resistencia LDR	
-			print a/k*5, 'V' # print voltaje en el divisor			
-			State = np.array([[1, estado(b/k)], [2, estado(c/k)]]) # array a guardar (para usar con np.savetxt)
-			np.savetxt('state', State, delimiter=" ", fmt="%s") # garda el txt actualizado
+				lec += a
+		lec = lec/k
+		print lec
+		Th = int(datetime.datetime.now().strftime('%H'))
+		Tm = int(datetime.datetime.now().strftime('%M'))
+		if lec<0.1: # lec<0.1 hay persona	 
+			if Th>=13 or Th<=7:
+				state = 1		
+			else:
+				state = 0
+		else:
+			state = 0
+		if k!=0:		
+			State = np.array([[1, estado(state)]]) # array a guardar (para usar con np.savetxt)
+			np.savetxt('state', State, delimiter=" ", fmt="%s") # garda el txt actualizado	
+		D2.write(state)
 		t1 = time.time() # nuevo tiempo inicial
